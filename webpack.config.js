b40b27e1e4c50d6913894c429e08b778d23d8559
yeyoung8805 <yyk8805@naver.com>
2022-08-24 16:23:50 +0900

    set webpack.config.js for using webpack bundler

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+//import
+const path = require("path");
+
+//export
+module.exports = {
+  //parcel index.html 과 같이 parcel main.js 가 진입점 기준이 된다.
+  //파일을 읽어들이기 시작하는 진입점 설정
+  entry: "./js/main.js",
+  //결과물(번들)을 반환하는 설정
+  output: {
+    path: "dist",
+    filename: "main.js",
+  },
+};
