d7b07660f2d7fbd848246734bad4ecb4034a669a
yeyoung8805 <yyk8805@naver.com>
2022-08-24 16:58:13 +0900

    call HtmlPlugin to make use of root index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 //import
 const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
 
 //export
 module.exports = {
@@ -13,4 +14,11 @@ module.exports = {
     // filename: "main.js",
     clean: true,
   },
+
+  //번들링 후 결과물의 처리방식 등 다양한 플러그인들을 설정
+  plugins: [
+    new HtmlPlugin({
+      template: "./index.html",
+    }),
+  ],
 };
